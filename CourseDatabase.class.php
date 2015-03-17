7f2d3bd767aa18fa10c142a305ab9255f3f091ff
Rasmus Fuhse <fuhse@data-quest.de>
2015-03-17 15:43:19 +0100

    new plus-page info

--- CourseDatabase.class.php
+++ CourseDatabase.class.php
@@ -4,17 +4,6 @@ require_once __DIR__."/models/CourseDB.class.php";
 
 class CourseDatabase extends StudIPPlugin implements StandardPlugin {
 
-    function getMetadata() {
-        $metadata = parent::getMetadata();
-        $metadata['description'] = "
-Stellen Sie echte Datenbanken zur Verfügung, die mit einem granularen Rechtemanagement entweder nur für Sie als DozentIn, für alle Teilnehmer des Kurses oder einzelne Teilnehmergruppen freigegeben werden.
-
-Damit können Noten erfasst werden, man kann statistische Daten zusammen tragen oder einfach einen Einsteigerkurs zum Thema relationale Datenbanken halten.
-        ";
-        $metadata['homepage'] = "https://github.com/Krassmus/CourseDatabase";
-        return $metadata;
-    }
-
     public function __construct() {
         parent::__construct();
     }
@@ -22,7 +11,7 @@ Damit können Noten erfasst werden, man kann statistische Daten zusammen tragen o
     function getTabNavigation($course_id) {
         $databases = CourseDB::findMine($_SESSION['SessionSeminar']);
         if (count($databases) || $GLOBALS['perm']->have_studip_perm("tutor", $course_id)) {
-            $tab = new Navigation(_("Data"), PluginEngine::getURL($this, array(), "manager/overview"));
+            $tab = new Navigation(_("Datenbanken"), PluginEngine::getURL($this, array(), "manager/overview"));
             $tab->setImage($this->getPluginURL()."/assets/database_white.svg");
             return array('coursedatabase' => $tab);
         } else {
@@ -37,7 +26,7 @@ Damit können Noten erfasst werden, man kann statistische Daten zusammen tragen o
     function getIconNavigation($course_id, $last_visit, $user_id) {
         $databases = CourseDB::findMine($course_id);
         if (count($databases) || $GLOBALS['perm']->have_studip_perm("tutor", $course_id)) {
-            $tab = new Navigation(_("Data"), PluginEngine::getURL($this, array(), "manager/overview"));
+            $tab = new Navigation(_("Datenbanken"), PluginEngine::getURL($this, array(), "manager/overview"));
             $tab->setImage($this->getPluginURL()."/assets/database_grey.png", array('title' => _("Datenbanken")));
             return $tab;
         } else {
